7862bf35cf420c91f4bd76602636ec411999bb4c
Niklas <niklas.pierick@stud.thga.de>
2023-09-28 12:33:47 +0200

    Test

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,7 @@
 #include <fmt/core.h>
 #include <fmt/format.h>
 #include <fstream>
-#include <imgui.h>
+    #include <imgui.h   >
 #include <nlohmann/json.hpp>
 #include <nlohmann/json_fwd.hpp>
 #include <set>
