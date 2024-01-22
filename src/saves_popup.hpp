be15738a9088c262225969e45ba8a3672538bff0
anna.theimann@outlook.de <anna.theimann@outlook.de>
2024-01-22 10:43:46 +0100

    hpp and cpp in the right format

--- src/saves_popup.hpp
+++ src/saves_popup.hpp
@@ -1,13 +1,14 @@
 #pragma once
 #include "../ai_omniscope-v2-communication_sw/src/OmniscopeSampler.hpp"
-#include <nlohmann/json.hpp>
 #include <fstream>
+#include <nlohmann/json.hpp>
 
 namespace fs = std::filesystem;
 
-static void save(std::map<Omniscope::Id, std::vector<std::pair<double, double>>> const
-                &alignedData, fs::path const &outFile)
-{
+static void
+save(std::map<Omniscope::Id, std::vector<std::pair<double, double>>> const
+         &alignedData,
+     fs::path const &outFile) {
   auto minSize = std::numeric_limits<std::size_t>::max();
 
   std::vector<std::vector<std::pair<double, double>> const *> data;
@@ -15,8 +16,7 @@ static void save(std::map<Omniscope::Id, std::vector<std::pair<double, double>>>
   std::string fileContent;
 
   for (auto sep = std::string_view{};
-       auto const &[device, values] : alignedData)
-  {
+       auto const &[device, values] : alignedData) {
     fileContent += sep;
     fileContent += "\"";
     fileContent += fmt::format("{}-{}", device.type, device.serial);
@@ -27,16 +27,13 @@ static void save(std::map<Omniscope::Id, std::vector<std::pair<double, double>>>
   }
   fileContent += '\n';
 
-  for (std::size_t i{}; i < minSize; ++i)
-  {
+  for (std::size_t i{}; i < minSize; ++i) {
     fileContent += fmt::format("{}", i);
     fileContent += ",";
-    for (auto sep = std::string_view{}; auto const &vec : data)
-    {
+    for (auto sep = std::string_view{}; auto const &vec : data) {
       auto const dataPair = (*vec)[i];
       fileContent += sep;
-      if (dataPair.second)
-      {
+      if (dataPair.second) {
         fileContent += fmt::format("{}", dataPair.second);
       }
       sep = ",";
@@ -66,8 +63,7 @@ void saves_popup(nlohmann::json const &config, nlohmann::json const &language,
                      &captureData,
                  std::chrono::system_clock::time_point &now,
                  std::time_t &now_time_t, std::tm &now_tm, std::string &path,
-                 bool &flagDataNotSaved)
-{
+                 bool &flagDataNotSaved) {
   ImGui::SetItemDefaultFocus();
 
   // Buffers
@@ -76,14 +72,14 @@ void saves_popup(nlohmann::json const &config, nlohmann::json const &language,
   static char mileage[10] = "";
   static char scantype[255] = "";
 
-  ImGui::InputText("StoragePathInputField1", storagePath_1, sizeof(storagePath_1));
+  ImGui::InputText("StoragePathInputField1", storagePath_1,
+                   sizeof(storagePath_1));
   ImGui::SameLine();
 
   static bool b1 = false;
   static bool b2 = false;
 
-  if (ImGui::BeginCombo("", "DevicesMenu"))
-  {
+  if (ImGui::BeginCombo("", "DevicesMenu")) {
     ImGui::Text("");
     ImGui::Checkbox("Device1", &b1);
     ImGui::Checkbox("Device2", &b2);
@@ -111,13 +107,11 @@ void saves_popup(nlohmann::json const &config, nlohmann::json const &language,
   // ############################ Popup Storage Path Input Field 2
   // ##############################
   if (ImGui::BeginPopupModal("StoragePathInputField2", nullptr,
-                             ImGuiWindowFlags_AlwaysAutoResize))
-  {
+                             ImGuiWindowFlags_AlwaysAutoResize)) {
     ImGui::SetItemDefaultFocus();
     ImGui::InputTextWithHint("", "Enter Storage Path Input Field 2",
                              storagePath_2, IM_ARRAYSIZE(storagePath_2));
-    if (ImGui::Button("Cancel"))
-    {
+    if (ImGui::Button("Cancel")) {
       storagePath_2[0] = 0;
       ImGui::CloseCurrentPopup();
     }
@@ -131,8 +125,7 @@ void saves_popup(nlohmann::json const &config, nlohmann::json const &language,
   // ############# End popup
 
   // create a .csv file name
-  auto fileName = [&](const std::string &name)
-  {
+  auto fileName = [&](const std::string &name) {
     now = std::chrono::system_clock::now();
     now_time_t = std::chrono::system_clock::to_time_t(now);
     now_tm = *std::gmtime(&now_time_t);
@@ -141,14 +134,13 @@ void saves_popup(nlohmann::json const &config, nlohmann::json const &language,
   };
 
   // save data to the file
-  auto saveStoragePath = [&](const std::string &fileContent, const fs::path &outFile)
-  {
+  auto saveStoragePath = [&](const std::string &fileContent,
+                             const fs::path &outFile) {
     fs::path target_path{fs::current_path() / outFile};
     std::fstream file;
     file.open(target_path, std::ios::out | std::ios::app);
 
-    if (!file.is_open())
-    {
+    if (!file.is_open()) {
       file.clear();
       fmt::print("Could not create {} for writing!\n", outFile.string());
       return;
@@ -161,8 +153,7 @@ void saves_popup(nlohmann::json const &config, nlohmann::json const &language,
     fmt::print("Finished saving\n");
   };
 
-  auto makeDirectory = [&](const fs::path &second_folder)
-  {
+  auto makeDirectory = [&](const fs::path &second_folder) {
     auto temp_path = second_folder;
     temp_path = temp_path.lexically_normal();
     temp_path = temp_path.relative_path();
@@ -176,15 +167,13 @@ void saves_popup(nlohmann::json const &config, nlohmann::json const &language,
   };
 
   if (ImGui::Button(load_json<std::string>(language, "button", "save").c_str(),
-                    ImVec2(load_json<Size>(config, "button"))))
-  {
+                    ImVec2(load_json<Size>(config, "button")))) {
     flagDataNotSaved = false;
     std::string filename = fileName(mileage);
 
     if (captureData.empty())
       ImGui::CloseCurrentPopup();
-    else
-    {
+    else {
       auto complete_path = makeDirectory(fs::path(inputvin) / scantype);
       save(captureData, path / complete_path / filename);
       ImGui::CloseCurrentPopup();
@@ -192,27 +181,22 @@ void saves_popup(nlohmann::json const &config, nlohmann::json const &language,
   }
 
   ImGui::SameLine();
-  if (ImGui::Button(load_json<std::string>(language, "button", "back").c_str(), {0, 0}))
+  if (ImGui::Button(load_json<std::string>(language, "button", "back").c_str(),
+                    {0, 0}))
     ImGui::CloseCurrentPopup();
 
-  auto storagePath = [=]()
-  {
+  auto storagePath = [=]() {
     ImGui::SameLine();
     ImGui::Dummy({550, 0});
     ImGui::SameLine();
-    if (ImGui::Button("Save Path"))
-    {
+    if (ImGui::Button("Save Path")) {
       fs::path complete_path1, complete_path2;
 
-      if (b1 && b2)
-      {
-        if (path[0] == '\0')
-        {
+      if (b1 && b2) {
+        if (path[0] == '\0') {
           complete_path1 = makeDirectory(storagePath_1);
           complete_path2 = makeDirectory(storagePath_2);
-        }
-        else
-        {
+        } else {
           complete_path1 = makeDirectory(fs::path(path) / storagePath_1);
           complete_path2 = makeDirectory(fs::path(path) / storagePath_2);
         }
@@ -222,9 +206,7 @@ void saves_popup(nlohmann::json const &config, nlohmann::json const &language,
         storagePath_1[0] = 0;
         saveStoragePath(full_content, complete_path2 / filename2);
         storagePath_2[0] = 0;
-      }
-      else
-      {
+      } else {
         if (path[0] == '\0')
           complete_path1 = makeDirectory(storagePath_1);
         else
@@ -241,14 +223,13 @@ void saves_popup(nlohmann::json const &config, nlohmann::json const &language,
     storagePath();
 
   // if both devices are selected
-  else if (b1 && b2)
-  {
+  else if (b1 && b2) {
     storagePath();
     ImGui::SameLine();
     ImGui::PushStyleVar(ImGuiStyleVar_FrameRounding, 100);
     if (ImGui::Button(" + "))
       ImGui::OpenPopup("StoragePathInputField2");
-      
+
     ImGui::PopStyleVar();
     ImGui::Dummy({850, 0});
     ImGui::SameLine();
