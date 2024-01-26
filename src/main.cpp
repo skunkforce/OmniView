f381389c551c14ad99d75e464d9bdcd790e6d93e
akmaily <anna.theimann@outlook.de>
2024-01-26 13:34:33 +0100

    Added a Style.hpp and set comments for the main function

--- src/main.cpp
+++ src/main.cpp
@@ -27,93 +27,13 @@
 #include "sendData.hpp"
 #include <regex>
 
-namespace ImGui {
-inline void SetupImGuiStyle(bool bStyleDark_, float alpha_) {
-  ImGuiStyle &style = ImGui::GetStyle();
+// include style 
 
-  // light style from Pacôme Danhiez (user itamago)
-  // https://github.com/ocornut/imgui/pull/511#issuecomment-175719267
-  style.Alpha = 1.0f;
-  style.FrameRounding = 3.0f;
-  style.Colors[ImGuiCol_Text] = ImVec4(0.00f, 0.00f, 0.00f, 1.00f);
-  style.Colors[ImGuiCol_TextDisabled] = ImVec4(0.60f, 0.60f, 0.60f, 1.00f);
-  style.Colors[ImGuiCol_WindowBg] = ImVec4(0.94f, 0.94f, 0.94f, 0.94f);
-  // style.Colors[ImGuiCol_ChildWindowBg]         = ImVec4(0.00f, 0.00f, 0.00f,
-  // 0.00f);
-  style.Colors[ImGuiCol_PopupBg] = ImVec4(1.00f, 1.00f, 1.00f, 0.94f);
-  style.Colors[ImGuiCol_Border] = ImVec4(0.00f, 0.00f, 0.00f, 0.39f);
-  style.Colors[ImGuiCol_BorderShadow] = ImVec4(1.00f, 1.00f, 1.00f, 0.10f);
-  style.Colors[ImGuiCol_FrameBg] = ImVec4(1.00f, 1.00f, 1.00f, 0.94f);
-  style.Colors[ImGuiCol_FrameBgHovered] = ImVec4(0.26f, 0.59f, 0.98f, 0.40f);
-  style.Colors[ImGuiCol_FrameBgActive] = ImVec4(0.26f, 0.59f, 0.98f, 0.67f);
-  style.Colors[ImGuiCol_TitleBg] = ImVec4(0.96f, 0.96f, 0.96f, 1.00f);
-  style.Colors[ImGuiCol_TitleBgCollapsed] = ImVec4(1.00f, 1.00f, 1.00f, 0.51f);
-  style.Colors[ImGuiCol_TitleBgActive] = ImVec4(0.82f, 0.82f, 0.82f, 1.00f);
-  style.Colors[ImGuiCol_MenuBarBg] = ImVec4(0.86f, 0.86f, 0.86f, 1.00f);
-  style.Colors[ImGuiCol_ScrollbarBg] = ImVec4(0.98f, 0.98f, 0.98f, 0.53f);
-  style.Colors[ImGuiCol_ScrollbarGrab] = ImVec4(0.69f, 0.69f, 0.69f, 1.00f);
-  style.Colors[ImGuiCol_ScrollbarGrabHovered] =
-      ImVec4(0.59f, 0.59f, 0.59f, 1.00f);
-  style.Colors[ImGuiCol_ScrollbarGrabActive] =
-      ImVec4(0.49f, 0.49f, 0.49f, 1.00f);
-  style.Colors[ImGuiCol_PopupBg] = ImVec4(0.86f, 0.86f, 0.86f, 0.99f);
-  style.Colors[ImGuiCol_CheckMark] = ImVec4(0.26f, 0.59f, 0.98f, 1.00f);
-  style.Colors[ImGuiCol_SliderGrab] = ImVec4(0.24f, 0.52f, 0.88f, 1.00f);
-  style.Colors[ImGuiCol_SliderGrabActive] = ImVec4(0.26f, 0.59f, 0.98f, 1.00f);
-  style.Colors[ImGuiCol_Button] = ImVec4(0.26f, 0.59f, 0.98f, 0.40f);
-  style.Colors[ImGuiCol_ButtonHovered] = ImVec4(0.26f, 0.59f, 0.98f, 1.00f);
-  style.Colors[ImGuiCol_ButtonActive] = ImVec4(0.06f, 0.53f, 0.98f, 1.00f);
-  style.Colors[ImGuiCol_Header] = ImVec4(0.26f, 0.59f, 0.98f, 0.31f);
-  style.Colors[ImGuiCol_HeaderHovered] = ImVec4(0.26f, 0.59f, 0.98f, 0.80f);
-  style.Colors[ImGuiCol_HeaderActive] = ImVec4(0.26f, 0.59f, 0.98f, 1.00f);
-  // style.Colors[ImGuiCol_Column]                = ImVec4(0.39f, 0.39f,
-  // 0.39f, 1.00f); style.Colors[ImGuiCol_ColumnHovered]         = ImVec4(0.26f,
-  // 0.59f, 0.98f, 0.78f); style.Colors[ImGuiCol_ColumnActive]          =
-  // ImVec4(0.26f, 0.59f, 0.98f, 1.00f);
-  style.Colors[ImGuiCol_ResizeGrip] = ImVec4(1.00f, 1.00f, 1.00f, 0.50f);
-  style.Colors[ImGuiCol_ResizeGripHovered] = ImVec4(0.26f, 0.59f, 0.98f, 0.67f);
-  style.Colors[ImGuiCol_ResizeGripActive] = ImVec4(0.26f, 0.59f, 0.98f, 0.95f);
-  // style.Colors[ImGuiCol_CloseButton]           = ImVec4(0.59f, 0.59f, 0.59f,
-  // 0.50f); style.Colors[ImGuiCol_CloseButtonHovered]    = ImVec4(0.98f, 0.39f,
-  // 0.36f, 1.00f); style.Colors[ImGuiCol_CloseButtonActive]     = ImVec4(0.98f,
-  // 0.39f, 0.36f, 1.00f);
-  style.Colors[ImGuiCol_PlotLines] = ImVec4(0.39f, 0.39f, 0.39f, 1.00f);
-  style.Colors[ImGuiCol_PlotLinesHovered] = ImVec4(1.00f, 0.43f, 0.35f, 1.00f);
-  style.Colors[ImGuiCol_PlotHistogram] = ImVec4(0.90f, 0.70f, 0.00f, 1.00f);
-  style.Colors[ImGuiCol_PlotHistogramHovered] =
-      ImVec4(1.00f, 0.60f, 0.00f, 1.00f);
-  style.Colors[ImGuiCol_TextSelectedBg] = ImVec4(0.26f, 0.59f, 0.98f, 0.35f);
-  // style.Colors[ImGuiCol_ModalWindowDarkening]  = ImVec4(0.20f, 0.20f, 0.20f,
-  // 0.35f);
+#include "menus/Style.hpp"
 
-  if (bStyleDark_) {
-    for (int i = 0; i < ImGuiCol_COUNT; i++) {
-      ImVec4 &col = style.Colors[i];
-      float H, S, V;
-      ImGui::ColorConvertRGBtoHSV(col.x, col.y, col.z, H, S, V);
+//include menus
 
-      if (S < 0.1f) {
-        V = 1.0f - V;
-      }
-      ImGui::ColorConvertHSVtoRGB(H, S, V, col.x, col.y, col.z);
-      if (col.w < 1.00f) {
-        col.w *= alpha_;
-      }
-    }
-  } else {
-    // std::cout << ImGuiCol_COUNT << std::endl;
-    for (int i = 0; i < ImGuiCol_COUNT; i++) {
-      ImVec4 &col = style.Colors[i];
-      if (col.w < 1.00f) {
-        col.x *= alpha_;
-        col.y *= alpha_;
-        col.z *= alpha_;
-        col.w *= alpha_;
-      }
-    }
-  }
-}
-} // namespace ImGui
+
 
 static std::vector<std::string>
 getAvailableLanguages(std::string const &languageFolder) {
@@ -159,11 +79,14 @@ static void load_settings(nlohmann::json const &config) {
   io.FontGlobalScale = load_json<float>(config, "text", "scale");
 }
 
+
 // ###########################################################################
 // ############# INT MAIN BEGINN #############################################
 // ###########################################################################
 
 int main() {
+
+  // Loading the config and language files 
   nlohmann::json config;
   const std::string configpath = "config/config.json";
   if (std::filesystem::exists(configpath)) {
@@ -196,6 +119,8 @@ int main() {
   static constexpr int VID = 0x2e8au;
   static constexpr int PID = 0x000au;
   // static constexpr std::size_t captureDataReserve = 1 << 26;
+
+  // Creating a device manager 
   OmniscopeDeviceManager deviceManager{};
   std::vector<std::shared_ptr<OmniscopeDevice>>
       devices; // = deviceManager.getDevices(VID, PID);
@@ -236,6 +161,8 @@ int main() {
 
   std::string path;
   path.resize(256);
+
+  // creating the plot function 
 
   auto addPlots = [&, firstRun = std::set<std::string>{}](
                       auto const &name, auto const &plots,
@@ -326,6 +253,9 @@ int main() {
     return 1;              // discard exceeding chars
   };
 
+
+  // creating the render function --> 
+  // Only the minimally necessary code should reside directly in this function; everything else should be structured and externalized into .hpp files.
   auto render = [&]() {
     load_settings(config);
     ImGui::SetupImGuiStyle(false, 0.99f);
@@ -336,9 +266,11 @@ int main() {
                      ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove); //
 
     // ############################ Menu bar ##############################
-    //  main menu
+    //  main menu -->will be changed to SideBarmenu.hpp --> seperate file 
     ImGui::BeginMainMenuBar();
 
+
+    // --> Initiliazing the language --> seperate file 
     std::string analyse_data{"analyse data"};
     std::string create_training_data{"create training data"};
     // replace space chars with new line
@@ -399,6 +331,7 @@ if (ImGui::BeginMenu(
         ImGui::EndMenu();
       }
 
+
       // greyed out color style
       ImGui::PushStyleColor(
           ImGuiCol_Text, load_json<Color>(config, "text", "color", "inactive"));
@@ -426,6 +359,8 @@ if (ImGui::BeginMenu(
 
     mainMenuBarSize = ImGui::GetItemRectSize();
     ImGui::EndMainMenuBar();
+
+    // EndSideBarMenu
 
     // ############################ Live Capture
     // ##############################
