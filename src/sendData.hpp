1bae84b820b7a39b91f75b4f3ea6e51299227513
R-Abbasi <s.rabbasi@yahoo.com>
2024-03-21 13:41:26 +0330

    receiving analyzed waveform and saving it

--- src/sendData.hpp
+++ src/sendData.hpp
@@ -4,47 +4,59 @@
 
 #include <curl/curl.h>
 #include <fmt/format.h>
+#include <string>
 
-inline std::string sendData(const std::string &data)
+inline size_t cb_write(void* contents, size_t size, size_t nmemb, void* stream) {
+    ((std::string*)stream)->append((char*)contents, size * nmemb);
+    return size * nmemb;
+}
+
+inline std::string sendData(const std::string& data)
 {
-  std::string api_message = "empty";
+    std::string api_message = "empty";
 
-  // init the winsock stuff (Windows)
-  curl_global_init(CURL_GLOBAL_ALL);
+    // init the winsock stuff (Windows)
+    curl_global_init(CURL_GLOBAL_ALL);
 
-  // get a curl handle
-  CURL* curl = curl_easy_init();
+    // get a curl handle
+    CURL* curl = curl_easy_init();
 
-  if (curl) {
-      curl_easy_setopt(curl, CURLOPT_URL, "https://api.auto-intern.de/check-request");
+    if (curl) {
+        curl_easy_setopt(curl, CURLOPT_URL, "https://api.auto-intern.de/analyze");
 
-      struct curl_slist* headers = NULL;
-      headers = curl_slist_append(headers, "Content-Type: application/json");
-      if (!headers) {
-          fmt::println("Failed to append the string!");
-          return api_message;
-      }
-          
-      curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+        struct curl_slist* headers = NULL;
+        headers = curl_slist_append(headers, "Content-Type: application/json");
+        if (!headers) {
+            fmt::println("Failed to append the string!");
+            return api_message;
+        }
+        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
 
-      // specify the POST data
-      curl_easy_setopt(curl, CURLOPT_POSTFIELDS, data.c_str());
+        // specify the POST data
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, data.c_str());
 
-      // Perform the request, res will get the return code
-      CURLcode res = curl_easy_perform(curl);
+        // save the response from the server into a string
+        std::string response;
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, cb_write);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
 
-      // Check the request status
-      if (res != CURLE_OK)
-          api_message = appLanguage[Key::Upload_failure];
-      else
-          api_message = appLanguage[Key::Upload_success];
-      // always cleanup
-      curl_easy_cleanup(curl);
-      curl_slist_free_all(headers); // free the list again 
-  }
-  else fmt::println("Failed to get a curl handle.");
+        // Perform the request, res will get the return code
+        CURLcode res = curl_easy_perform(curl);
 
-  curl_global_cleanup();
-  return api_message;
+        // Check the request status
+        if (res != CURLE_OK)
+            fmt::println("{}", appLanguage[Key::Upload_failure]);
+        else {
+           // TODO: check the responce
+            api_message = response;
+        }
+
+        curl_slist_free_all(headers); // free the list again 
+        curl_easy_cleanup(curl);      // always cleanup   
+    }
+    else fmt::println("Failed to get a curl handle.");
+
+    curl_global_cleanup();
+    return api_message;
 }
 #endif
