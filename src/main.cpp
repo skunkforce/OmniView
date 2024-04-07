d19fa614b8f5a5f5b6c7b2675b9a09be72df90f6
akmaily <anna.theimann@outlook.de>
2024-04-07 11:31:08 +0200

    Trying to fix the PlayButton State Bug 3

--- src/main.cpp
+++ src/main.cpp
@@ -139,17 +139,19 @@ int main() {
           ImGui::PopStyleColor(3);
         }
       } else {
-        PngRenderedCnt = 0;
-        if (ImGui::ImageButton(
-                appLanguage[Key::Start],
-                (void *)(intptr_t)image_texture[PngRenderedCnt],
-                ImVec2(image_width[PngRenderedCnt] * iconsSacle,
-                       image_height[PngRenderedCnt] * iconsSacle))) {
-          ImGui::OpenPopup("No devices are selected");
+        if (!sampler.has_value()) {
+          PngRenderedCnt = 0;
+          if (ImGui::ImageButton(
+                  appLanguage[Key::Start],
+                  (void *)(intptr_t)image_texture[PngRenderedCnt],
+                  ImVec2(image_width[PngRenderedCnt] * iconsSacle,
+                         image_height[PngRenderedCnt] * iconsSacle))) {
+            ImGui::OpenPopup("No devices are selected");
+          }
+          info_popup("No devices are selected",
+                     appLanguage[Key::No_dvc_available]);
+          // set_button_style_to(config, "standart");*/
         }
-        info_popup("No devices are selected",
-                   appLanguage[Key::No_dvc_available]);
-        // set_button_style_to(config, "standart");*/
       }
     } else {
       PngRenderedCnt = 1;
