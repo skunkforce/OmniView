145c3c7ea71de038685cd2b73570c7baa2c0fc31
akmaily <anna.theimann@outlook.de>
2024-04-07 14:01:56 +0200

    Changed the VCDS language to German and the menu style

--- src/VCDS_data.hpp
+++ src/VCDS_data.hpp
@@ -21,16 +21,17 @@ static void show_standart_input(nlohmann::json const &config,
                                 std::string &inputvin_string,
                                 std::string &mileage_string,
                                 std::string &comment_string) {
-  static std::string inputvin;
+  static char inputvin[18];
   static char mileage[10];
   static char comment[1000];
 
   ImVec2 windowSize = ImGui::GetWindowSize();
-  ImGui::BeginChild("trainingleft",
-                    ImVec2(windowSize.x * 0.5f, windowSize.y * 0.8f));
-  ImGui::TextUnformatted("base_data");
-  inputvin = "jkasldfh";
-  ImGui::InputText("label", mileage, sizeof(mileage));
+  ImGui::Text("Geben sie die benötigten Eingaben zur Datei ein und wählen sie "
+              "ihre VCDS-Datei aus");
+  // ImGui::BeginChild("trainingleft",
+  //                 ImVec2(windowSize.x * 0.5f, windowSize.y * 0.8f));
+  ImGui::InputText("VIN", inputvin, sizeof(inputvin));
+  ImGui::InputText("Kilometerstand", mileage, sizeof(mileage));
   static bool problem = false;
   static bool maintenance = !problem;
 
@@ -39,40 +40,40 @@ static void show_standart_input(nlohmann::json const &config,
 
   static bool expected = true;
   static bool anomaly = !expected;
-  ImGui::TextUnformatted("reason");
-  ImGui::Checkbox("maintenance", &maintenance);
+  ImGui::TextUnformatted("Grund");
+  ImGui::Checkbox("Wartung", &maintenance);
   if (maintenance == problem) {
     problem = !maintenance;
   }
   ImGui::SameLine();
-  ImGui::Checkbox("problem", &problem);
+  ImGui::Checkbox("Problem", &problem);
   if (problem == maintenance) {
     maintenance = !problem;
   }
 
-  ImGui::TextUnformatted("electrical consumer");
-  ImGui::Checkbox("switched off", &electric_off);
+  ImGui::TextUnformatted("Elektrische Konsument");
+  ImGui::Checkbox("aus", &electric_off);
   if (electric_off == electric_on) {
     electric_on = !electric_off;
   }
   ImGui::SameLine();
-  ImGui::Checkbox("switched on", &electric_on);
+  ImGui::Checkbox("an", &electric_on);
   if (electric_on == electric_off) {
     electric_off = !electric_on;
   }
-  ImGui::TextUnformatted("evaluation");
-  ImGui::Checkbox("normal_case", &expected);
+  ImGui::TextUnformatted("Bewertung");
+  ImGui::Checkbox("Normalfall", &expected);
   if (expected == anomaly) {
     anomaly = !expected;
   }
   ImGui::SameLine();
-  ImGui::Checkbox("anomaly", &anomaly);
+  ImGui::Checkbox("Anomalie", &anomaly);
   if (anomaly == expected) {
     expected = !anomaly;
   }
-  ImGui::InputTextMultiline("comment", comment, sizeof(comment));
+  ImGui::InputTextMultiline("Kommentar", comment, sizeof(comment));
 
-  ImGui::EndChild();
+  // ImGui::EndChild();
   inputvin_string = inputvin;
   mileage_string = mileage;
   comment_string = comment;
@@ -85,7 +86,8 @@ static void selected_vcds_data(nlohmann::json const &config,
                                nlohmann::json &metadata, std::string &inputvin,
                                std::string &mileage, std::string &comment,
                                std::string &api_message, bool &upload_success) {
-  ImGui::TextUnformatted("vcds");
+
+  // ImGui::TextUnformatted("vcds");
   static ImGui::FileBrowser fileBrowser;
   static bool first_job = true;
   static bool flagApiSending = false;
@@ -100,7 +102,7 @@ static void selected_vcds_data(nlohmann::json const &config,
 
   ImGui::InputText("##path1", path1, sizeof(path1));
   ImGui::SameLine();
-  if (ImGui::Button(fmt::format("{} 1", "file").c_str())) {
+  if (ImGui::Button("Durchsuchen")) {
     fileBrowser.Open();
   }
   fileBrowser.Display();
@@ -151,14 +153,11 @@ static void selected_vcds_data(nlohmann::json const &config,
       ImGui::Text("senden... %c", "|/-\\"[(int)(ImGui::GetTime() / 0.05f) & 3]);
     }
   }
-  ImGui::EndChild();
+  // ImGui::EndChild();
 }
 
 inline void popup_create_training_data_select(nlohmann::json const &config,
                                               bool &upload_success) {
-  static int selectedOption = 0; // Standardauswahl
-  const char *options[] = {"VCDS-Datei"};
-  ImGui::Combo("Messung", &selectedOption, options, IM_ARRAYSIZE(options));
 
   static std::string inputvin = "";
   static std::string mileage = "";
@@ -168,18 +167,12 @@ inline void popup_create_training_data_select(nlohmann::json const &config,
   ImGui::SetItemDefaultFocus();
   show_standart_input(config, metadata, inputvin, mileage, comment);
   ImGui::SameLine();
-
   ImVec2 windowSize = ImGui::GetWindowSize();
-  ImGui::BeginChild("trainingright",
-                    ImVec2(windowSize.x * 0.5f, windowSize.y * 0.8f));
-  switch (selectedOption) {
-  case 0:
-    selected_vcds_data(config, metadata, inputvin, mileage, comment,
-                       api_message, upload_success);
-    break;
-  default:
-    ImGui::Text("Fehler, Switchcase mit unerwarteter Auswahl");
-  }
+  // ImGui::BeginChild("trainingright",
+  //                 ImVec2(windowSize.x * 0.5f, windowSize.y * 0.8f));
+  selected_vcds_data(config, metadata, inputvin, mileage, comment, api_message,
+                     upload_success);
+  ImGui::SameLine();
   if (ImGui::Button("back", ImVec2(load_json<Size>(config, "button")))) {
     ImGui::CloseCurrentPopup();
   }
