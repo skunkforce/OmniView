8ef1032c10aabb7f539db9f497d8327e44d35cac
R-Abbasi <s.rabbasi@yahoo.com>
2024-05-15 11:50:51 +0330

    gcc 13

--- src/saves_popup.cpp
+++ src/saves_popup.cpp
@@ -2,7 +2,6 @@
 #include <sstream>
 #include <future>
 #include <charconv>
-#include <print> // testing purposes
 #include "popups.hpp"
 #include "look_up_saves.hpp"
 #include "imgui_stdlib.h"
@@ -15,8 +14,8 @@ static void save(const Omniscope::Id &device,
                  const std::vector<std::pair<double, double>> &values,
                  const fs::path &outFile, std::string allData, size_t &y_indx) {
 
-  allData += fmt::format("\nType: {}\nSerial: {}\nSamplyingRate: {}\n", device.type,
-                        device.serial, device.sampleRate);
+  allData += fmt::format("\nType: {}\nSerial: {}\nSamplyingRate: {}\n",
+                         device.type, device.serial, device.sampleRate);
   std::string fileContent;
   fileContent.resize_and_overwrite(
       // five bytes for each y_value, three for the number
