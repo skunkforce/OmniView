d6fe1cd0f8c74aa5bffdde9cb8a1a2559b88998b
arkadiusz <arkadius.bulinski@gmail.com>
2024-07-18 12:06:06 +0200

    Add connection management and message forwarding in the WebSocket server
    - Add 'std::vector' to track active WebSocket connections and 'std::mutex' for thread-safe access in 'ws.cpp'
    - Implement logic to add connections on 'onopen' event and remove them on 'onclose' event
    - Improve 'onmessage' event to forward incoming messages to all connected clients except the sender
    - Ensure thread-safe operations on the connection list with 'std::lock_guard'

--- ai_omniscope-websocket_sw/src/ws.cpp
+++ ai_omniscope-websocket_sw/src/ws.cpp
@@ -1,19 +1,36 @@
 #include "crow.h"
 #include "fmt/core.h"
+#include <vector>
+#include <mutex>
+
+std::vector<crow::websocket::connection*> connections;
+std::mutex connections_mutex;
 
 void configureWebSocketRoutes(crow::SimpleApp& app) {
     fmt::print("The WebSocket is started\n");
     CROW_WEBSOCKET_ROUTE(app, "/")
         .onopen([&](crow::websocket::connection& conn) {
+            std::lock_guard<std::mutex> lock(connections_mutex);
+            connections.push_back(&conn);
             fmt::print("WebSocket connection opended: {}\n", static_cast<void*>(&conn));
             
             })
         .onclose([&](crow::websocket::connection& conn, const std::string& reason) {
+            std::lock_guard<std::mutex> lock(connections_mutex);
+            connections.erase(std::remove(connections.begin(), connections.end(), &conn), connections.end());
             fmt::print("WebSocket connection closed: {}\n", static_cast<void*>(&conn));
 
             })
         .onmessage([&](crow::websocket::connection& conn, const std::string& data,  bool is_bunary) {
             fmt::print("Received message: {}\n", data);
+
+            // Forward the message to all connected clients
+            std::lock_guard<std::mutex> lock(connections_mutex);
+            for (auto& client_conn : connections) {
+                if (client_conn != &conn) {
+                    client_conn->send_text(data);
+                }
+            }
             });
 }
 
