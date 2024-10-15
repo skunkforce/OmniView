1a1de2ff7ba0ebe1a90fe96eabea438e29135413
AKMaily <anna.theimann@outlook.de>
2024-10-15 10:54:25 +0200

    TESTING: Local Build with imgui as submodule

--- mytoolchain/mytoolchain.cmake
+++
@@ -1,9 +0,0 @@
-# my_toolchain.cmake
-
-# Setze das Präprozessor-Makro ImDrawIdx auf unsigned int für C++-Dateien
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -DImDrawIdx=\\\"unsigned int\\\"")
-
-# Option: Falls du vcpkg benutzt, binde die vcpkg-Toolchain-Datei ein
-if(NOT DEFINED CMAKE_TOOLCHAIN_FILE)
-    include("${CMAKE_CURRENT_LIST_DIR}/../vcpkg/scripts/buildsystems/vcpkg.cmake")
-endif()
